9f0887773c5f728fbf3783dcf5625cd85a5224ef
Andrey Filipov <andrey@filipov.dev>
2025-03-14 03:09:07 +0300

    vue3-ts-pinia-example-6 Реализовать менеджер состояний (#7)

---
+++ src/stores/account.ts
@@ -0,0 +1,57 @@
+import { defineStore } from 'pinia';
+import { type Account } from '@/models/account.model';
+import { v4 as uuidv4 } from 'uuid';
+
+/**
+ * Хранилище для управления учетными записями
+ * @class AccountStore
+ */
+export const useAccountStore = defineStore('account', {
+  state: () => ({
+    /**
+     * Список учетных записей
+     * @type {Account[]}
+     */
+    accounts: [] as Account[],
+  }),
+  actions: {
+    /**
+     * Добавляет новую учетную запись
+     * @method addAccount
+     * @example
+     * store.addAccount();
+     */
+    addAccount() {
+      const newAccount = {
+        id: uuidv4(),
+        labels: [],
+        type: 'LDAP' as 'LDAP' | 'Local',
+        login: '',
+        password: null,
+      };
+
+      this.accounts.push(newAccount);
+    },
+    /**
+     * Удаляет учетную запись
+     * @method deleteAccount
+     * @param {String} id - Удаляет данные учетной записи
+     */
+    deleteAccount(id: string) {
+      this.accounts = this.accounts.filter((account) => account.id !== id);
+    },
+    /**
+     * Обновляет существующую учетную запись
+     * @method updateAccount
+     * @param {Account} updatedAccount - Обновленные данные учетной записи
+     * @throws {Error} Если запись не найдена
+     */
+    updateAccount(updatedAccount: Account) {
+      const index = this.accounts.findIndex((acc) => acc.id === updatedAccount.id);
+
+      if (index !== -1) {
+        this.accounts[index] = updatedAccount;
+      }
+    },
+  },
+});
